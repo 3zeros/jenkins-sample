eaa25b054d09f8d5414257163da2f0f9ee529e15
3zeros <to.yooeun@gmail.com>
2020-09-02 00:08:11 +0900

    04 groovy create

---
+++ pipeline/04_artifact.groovy
@@ -0,0 +1,29 @@
+pipeline {
+    agent any
+    environment {
+        fileName1 = "${FILE_NAME}.txt"
+        fileName2 = "AnotherFile.txt"
+        contextOfFile2 = "Here is file2"
+    }
+    stages {
+        stage('create file') {
+            steps {
+                echo 'create file 1,2'
+                writeFile(file: fileName1, text: "${OUTPUT_TEXT}")
+                writeFile(file: fileName2, text: contextOfFile2)
+            }
+        }
+        stage('archive aartifacts'){
+            steps {
+                echo 'save file 1, 2'
+                archiveArtifacts fileName1
+                archiveArtifacts fileName2
+            }
+        }
+    }
+    post {
+        success {
+            cleanWs()
+        }
+    }
+}

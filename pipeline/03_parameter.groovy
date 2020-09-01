f6b150895a8d85996df4527b0d7289cf926a694a
3zeros <to.yooeun@gmail.com>
2020-09-01 23:59:14 +0900

    edit file name

--- pipeline/03_parameter.groovy
+++ pipeline/03_parameter.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('create file') {
             steps {
-                writeFile(file: "${FILE_NAME}_txt", text: "${OUTPUT_TEXT}")
+                writeFile(file: "${FILE_NAME}.txt", text: "${OUTPUT_TEXT}")
                 sh 'ls -la'
                 echo 'it is one of the benefits when using pipeline'
                 sh "cat ${FILE_NAME}.txt"

073db768c805f2137f8474a912ad29a90b083dcd
3zeros <to.yooeun@gmail.com>
2020-09-02 10:13:24 +0900

    final change 05

--- pipeline/05_getFile.groovy
+++ pipeline/05_getFile.groovy
@@ -15,8 +15,7 @@ pipeline {
         }
         stage('copy artifacts'){
             steps{
-                copyArtifacts(projectName: "${param.COPY_SOURCE_PROJECT}")
-                echo "${param.COPY_SOURCE_PROJECT}"
+                copyArtifacts(projectName: "${params.COPY_SOURCE_PROJECT}")
             }
         }
         stage('find files'){
